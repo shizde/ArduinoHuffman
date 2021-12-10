2aaa91e723bf3472774d39c162c5f0ca88af1d36
Rafael Perez <rafaelsperez@vivaldi.net>
2021-12-10 20:29:54 +0000

    compress functions

---
+++ arduinoHuffman.h
@@ -0,0 +1,19 @@
+/*
+ * 
+ */
+
+#ifndef arduinoHuffman_H_
+#define arduinoHuffman_H_
+
+// Compression Dictionary Constant
+#define NUM_CHAR 256
+
+// Compression Functions
+void compress_file(char* dataset, char* filename);
+void compress_string(char *dataset);
+
+// Decompression Functions
+//void decompress_file(char *filename);
+//void decompmress_string(char *dataset);
+
+#endif /* arduinoHuffman_H_ */
